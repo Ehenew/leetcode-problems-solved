c515c5d2322d1449225b8bff58ed8e25d96fc094
Ehenew <ehenew16@gmail.com>
2025-04-29 11:14:12 +0300

    Find the winner of circular game, now i am starting to win 💪

---
+++ 1823-find-the-winner.py
@@ -0,0 +1,11 @@
+class Solution:
+    def findTheWinner(self, n: int, k: int) -> int:
+        players = list(range(1, n + 1))
+
+        start = 0
+
+        while len(players) > 1:
+            index = (start + k - 1) % len(players)
+            players.pop(index)
+            start = index
+        return players[0]
