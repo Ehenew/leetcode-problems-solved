b1bee815e4f8a2511908de13e3302085f93a0579
Ehenew Amogne <159142632+Ehenew@users.noreply.github.com>
2025-05-22 18:59:38 +0300

    Time: 2 ms (60.86%), Space: 17.8 MB (70.38%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.py
+++ 0389-find-the-difference/0389-find-the-difference.py
@@ -1,13 +1,11 @@
 class Solution:
     def findTheDifference(self, s: str, t: str) -> str:
-        freq1 = {}
-        freq2 = {}
+        freq = {}
+
         for char in s:
-            freq1[char] = freq1.get(char, 0) + 1
+            freq[char] = freq.get(char, 0) + 1
+
         for char in t:
-            freq2[char] = freq2.get(char, 0) + 1
-        
-        for char in t:
-            if freq2[char] != freq1.get(char, 0):
+            freq[char] = freq.get(char, 0) - 1
+            if freq[char] < 0:
                 return char
-            
