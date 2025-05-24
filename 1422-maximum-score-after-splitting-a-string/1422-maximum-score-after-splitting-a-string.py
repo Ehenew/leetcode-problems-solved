9cfd3bef26761cd8c1e65520ab26cd99ed9ed831
Ehenew Amogne <159142632+Ehenew@users.noreply.github.com>
2025-05-24 13:20:12 +0300

    Time: 7 ms (20.77%), Space: 17.7 MB (63.28%) - LeetHub

--- 1422-maximum-score-after-splitting-a-string/1422-maximum-score-after-splitting-a-string.py
+++ 1422-maximum-score-after-splitting-a-string/1422-maximum-score-after-splitting-a-string.py
@@ -1,6 +1,5 @@
 class Solution:
-    def maxScore(self, s: str) -> int:
-        
+    def maxScore(self, s: str) -> int:        
         max_count = 0
 
         for i in range(1, len(s)):
@@ -9,7 +8,7 @@ class Solution:
                 
             left_count = left.count('0')
             right_count = right.count('1')
-            
+
             max_count = max(max_count, left_count + right_count)
         return max_count
 
