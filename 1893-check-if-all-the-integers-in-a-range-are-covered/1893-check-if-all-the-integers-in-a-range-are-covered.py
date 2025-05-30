aff9405172650f2149047b5399509ef9fea3c3f6
Ehenew Amogne <159142632+Ehenew@users.noreply.github.com>
2025-05-30 21:17:33 +0300

    Time: 1 ms (26.53%), Space: 17.6 MB (99.12%) - LeetHub

--- 1893-check-if-all-the-integers-in-a-range-are-covered/1893-check-if-all-the-integers-in-a-range-are-covered.py
+++ 1893-check-if-all-the-integers-in-a-range-are-covered/1893-check-if-all-the-integers-in-a-range-are-covered.py
@@ -3,8 +3,8 @@ class Solution:
         
         for num in range(left, right + 1):
             found = False
-            for r in ranges:
-                if r[0] <= num <= r[1]:
+            for arr in ranges:
+                if arr[0] <= num <= arr[1]:
                     found = True
                     break
             if not found:
