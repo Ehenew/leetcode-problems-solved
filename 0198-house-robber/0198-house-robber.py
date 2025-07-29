3eabff3e076ce228bc84ee832685b1fbc253b3b0
Ehenew Amogne <159142632+Ehenew@users.noreply.github.com>
2025-07-29 22:42:16 +0300

    Time: 4 ms (3.41%), Space: 17.8 MB (54.31%) - LeetHub

---
+++ 0198-house-robber/0198-house-robber.py
@@ -0,0 +1,12 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        def dp(i):
+            if  i < 0:
+                return 0
+            
+            if i not in memo:
+                memo[i] = max(dp(i - 1), dp(i - 2) + nums[i])
+            return memo[i]
+
+        memo = {}
+        return dp(len(nums) - 1)
