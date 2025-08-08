54a5a720a5d6eeda334a899e404994e995be9623
Ehenew Amogne <159142632+Ehenew@users.noreply.github.com>
2025-08-08 22:46:30 +0300

    Time: 16 ms (3.68%), Space: 18.5 MB (5.05%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.py
+++ 0392-is-subsequence/0392-is-subsequence.py
@@ -1,46 +1,56 @@
 class Solution:
     def isSubsequence(self, s: str, t: str) -> bool:
         m, n = len(s), len(t)
-        memo = [[-1] * n for _ in range(m)]
+        dp = [[0] * (n+1) for _ in range(m+1)]
 
-        def dfs(i, j):
-            if i == m or j == n:
-                return 0
-
-            if memo[i][j] != -1:
-                return memo[i][j]
-
-            if s[i] == t[j]:
-                memo[i][j] = 1 + dfs(i + 1, j + 1)
-            else:
-                memo[i][j] = dfs(i, j + 1)
-            
-            return memo[i][j]
-
-        return dfs(0,0) == m
+        for i in range(m-1, -1, -1):
+            for j in range(n-1, -1, -1):
+                if s[i] == t[j]:
+                    dp[i][j] = 1 + dp[i+1][j+1]
+                else:
+                    dp[i][j] = dp[i][j+1]
+        
+        return dp[0][0] == m
+        
 
 
 
-
-
-
-
-
-
-
-
-
-
-        # m, n = len(s), len(t)
-
-        # dp = [[0] * (n+1) for _ in range(m+1)]
-
-        # for i in range(1,m):
-        #     for j in range(1,n):
-        #         if s[i - 1] == t[j - 1]:
-        #             dp[i][j] = 1 + dp[i - 1][j - 1] 
+        # for i in range(1, m+1):
+        #     for j in range(1, n+1):
+        #         if s[i-1] == t[j-1]:
+        #             dp[i][j] = 1 + dp[i-1][j-1]
         #         else:
-        #             dp[i][j] = dp[i][j - 1]
+        #             dp[i][j] = dp[i][j-1]
         
         # return dp[m][n] == m
+
+
+
+        # Top down
+        # m, n = len(s), len(t)
+        # memo = [[-1] * n for _ in range(m)]
+
+        # def dfs(i, j):
+        #     if i == m or j == n:
+        #         return 0
+
+        #     if memo[i][j] != -1:
+        #         return memo[i][j]
+
+        #     if s[i] == t[j]:
+        #         memo[i][j] = 1 + dfs(i + 1, j + 1)
+        #     else:
+        #         memo[i][j] = dfs(i, j + 1)
+            
+        #     return memo[i][j]
+
+        # return dfs(0,0) == m
+
+
+
+
+
+
+
+
 
