bddf6166430c5803bc17f36a90e20689e53b6163
Ehenew Amogne <ehenewamogne@gmail.com>
2025-10-22 20:52:09 +0300

    Time: 39 ms (83.98%), Space: 45.3 MB (95.84%) - LeetHub

---
+++ 1609-even-odd-tree/1609-even-odd-tree.py
@@ -0,0 +1,36 @@
+from collections import deque
+class Solution:
+    def isEvenOddTree(self, root: Optional[TreeNode]) -> bool:
+        if not root:
+            return True
+        
+        q = deque([root])
+        level = 0
+
+        while q:
+            size = len(q)
+            prev = None
+
+            for _ in range(size):
+                node = q.popleft()
+
+                if level % 2 == 0:
+                    if node.val % 2 == 0:
+                        return False
+                    if prev is not None and node.val <= prev:
+                        return False
+                else:
+                    if node.val % 2 != 0:
+                        return False
+                    if prev is not None and node.val >= prev:
+                        return False
+
+                prev = node.val
+
+                if node.left:
+                    q.append(node.left)
+                if node.right:
+                    q.append(node.right)
+
+            level += 1
+        return True
