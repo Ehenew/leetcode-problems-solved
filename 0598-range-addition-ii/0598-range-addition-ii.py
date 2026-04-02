c243d79ba6ac5c316821f3b0a84e2e5e0ba5ee33
Ehenew Amogne <ehenewamogne@gmail.com>
2026-04-02 13:31:23 +0300

    Time: 0 ms (100%), Space: 20.5 MB (91.37%) - LeetHub

---
+++ 0598-range-addition-ii/0598-range-addition-ii.py
@@ -0,0 +1,12 @@
+class Solution:
+    def maxCount(self, m, n, ops):
+        if not ops:
+            return m * n
+        
+        min_row = m
+        min_col = n
+        
+        for a, b in ops:
+            min_row = min(min_row, a)
+            min_col = min(min_col, b)        
+        return min_row * min_col
